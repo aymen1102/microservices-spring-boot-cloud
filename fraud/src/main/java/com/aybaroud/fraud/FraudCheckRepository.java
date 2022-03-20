c312dcee5bb0403d416697301893c71a7d408b3d
Aymen BAROUDI <baroudi.aymen@gmail.com>
2022-03-20 10:01:58 +0100

    add new features + fix bugs

--- fraud/src/main/java/com/aybaroud/fraud/FraudCheckRepository.java
+++ fraud/src/main/java/com/aybaroud/fraud/FraudCheckRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FraudCheckRepository extends JpaRepository<FraudCheck,Integer> {
+public interface FraudCheckRepository
+        extends JpaRepository<FraudCheck,Integer> {
 }
